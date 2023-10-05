b05e18724b5a0cf042f3f851363c0d4ed8208214
kita2222 <146186718+kita2222@users.noreply.github.com>
2023-10-05 13:30:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,39 @@
-function omikuji() {
-  let msg1;
-  let msg2;
-  let random1;
-  let random2;
+const reader = new NDEFReader();
 
-  random1 = Math.floor(Math.random() * 5);
-  console.log(random1);
+reader
+  .scan()
+  .then(() => {
+    reader.onerror = (event) => {
+      updateStatus(`Error: ${error}`);
+    };
+    reader.onreading = (event) => {
+      updateResult(event);
+    };
+  })
+  .catch((error) => {
+    updateStatus(`Error! Scan failed to start: ${error}.`);
+  });
 
-  const result = ['大吉', '中吉', '吉', '凶', '大凶'];
+const updateStatus = (str) => {
+  $("#status").empty();
+  $("#status").text(str);
+};
 
-  msg1 = result[random1];
+const updateResult = (obj) => {
+  //表示を一旦削除
+  $("#serialnumber").empty();
+  $("#type").empty();
+  $("#timeStamp").empty();
 
-  random2 = Math.floor(Math.random() * 10);
-  console.log(random2);
+  if (!obj) {
+    return obj;
+  }
+  
+  //ステータス表記を更新
+  updateStatus("Success");
 
-  const resultMsg = [
-      '今日はいいこと起きそう',
-      'コーラを呑みましょう。',
-      'ポテチを食べましょう。',
-      'バックアップを取っておきましょう。',
-      '忘れ物をしないように気をつけてください。',
-      '怪我には気をつけてください。',
-      'なくしたものが見つかるでしょう。',
-      'お金を無くすでしょう。',
-      '電車が遅延するでしょう。',
-      'スタバの新作を飲みましょう。',
-  ];
-
-  msg2 = resultMsg[random2];
-
-  document.getElementById('msgHeading').setAttribute('style', 'opacity: 1;');
-  setTimeout(function() {
-      document.getElementById('msg1').innerHTML = msg1;
-  }, 1000);
-  setTimeout(function() {
-      document.getElementById('msg2').innerHTML = msg2;
-  }, 2000);
-  document.getElementById('playBtn').setAttribute('disabled', 'disabled');
-  document.getElementById('resetBtn').removeAttribute('disabled');
-}
-
-function reset() {
-  document.getElementById('msgHeading').setAttribute('style', 'opacity: 0;');
-  document.getElementById('msg1').innerHTML = '';
-  document.getElementById('msg2').innerHTML = '';
-  document.getElementById('playBtn').removeAttribute('disabled');
-  document.getElementById('resetBtn').setAttribute('disabled', 'disabled');
-}
+  //読み取り内容に基づき内容を更新
+  $("#type").text(obj.type);
+  $("#serialnumber").text(obj.serialNumber);
+  $("#timeStamp").text(obj.timeStamp);
+};
